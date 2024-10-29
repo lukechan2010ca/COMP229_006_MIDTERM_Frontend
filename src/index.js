11ea1b08937438b5078939d9678f5890104ea03e
lukechan2010ca <lukechan2010ca@gmail.com>
2024-10-28 21:33:13 -0400

    install react-scripts

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Header />}>
           <Route index element={<Home />} />
-          <Route path="/projects" element={<Projects />} />
+          <Route path="/Projects" element={<Projects />} />
           <Route path="/About" element={<About />} />
         </Route>
       </Routes>
